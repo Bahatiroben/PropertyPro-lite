dabd89a2f0f467414ee1ca28ba94b4510f77e8d2
Bahati Robben <bahatiroben@gmail.com>
2019-07-07 15:07:34 +0200

    feature(flag): report an add as spam [works on 167120795]

---
+++ server/models/flagModels.js
@@ -0,0 +1,15 @@
+import uuid from 'uuid';
+import moment from 'moment';
+import data from '../data/data';
+
+class FlagModel {
+	flag(id, details) {
+		const newFlag = { ...details };
+		newFlag.created_on = moment.now();
+		newFlag.property_id = uuid.v4();
+		newFlag.id = uuid.v4();
+		return data.flags.push(newFlag);
+	}
+}
+
+export default FlagModel;
